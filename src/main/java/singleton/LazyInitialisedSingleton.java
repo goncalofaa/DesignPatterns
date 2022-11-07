520eadf4acf6943ab32b8aa47b7c782d4936d187
Gonçalo Anacleto <goncalo.anacleto@sky.uk>
2022-11-07 17:14:18 +0000

    Singleton

--- src/main/java/singleton/LazyInitialisedSingleton.java
+++ src/main/java/singleton/LazyInitialisedSingleton.java
@@ -6,10 +6,20 @@ public class LazyInitialisedSingleton {
 
     private LazyInitialisedSingleton() {}
 
+    private String name;
+
     public static synchronized LazyInitialisedSingleton getSingleton() {
         if(lazyInitialisedSingleton == null){
             lazyInitialisedSingleton = new LazyInitialisedSingleton();
         }
         return lazyInitialisedSingleton;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- src/main/java/singleton/SingletonApplication.java
+++ src/main/java/singleton/SingletonApplication.java
@@ -6,5 +6,7 @@ public class SingletonApplication {
         LazyInitialisedSingleton singleton1 = LazyInitialisedSingleton.getSingleton();
         LazyInitialisedSingleton singleton2 = LazyInitialisedSingleton.getSingleton();
         System.out.println(singleton2.equals(singleton1));
+        singleton1.setName("Carlos");
+        System.out.println(singleton2.getName());
     }
 }
